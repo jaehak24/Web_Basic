4670af80c6f0661633e1d895215455503fd69ba4
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2023-01-31 21:59:34 +0900

    컨트롤러

---
+++ demo/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Rest api를 처리하는 Controller
+public class ApiController {
+}
