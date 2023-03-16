2d3310f094899b435951592e5981a320ccb64fb4
Jaehak Lee <65396939+jaehak24@users.noreply.github.com>
2023-03-16 21:32:45 +0900

    이전 복습

---
+++ ControllerPractice/src/main/java/com/example/controllerpractice/ControllerPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.controllerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerPracticeApplication.class, args);
+    }
+
+}
